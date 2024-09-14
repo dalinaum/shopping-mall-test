569d549e2e463b987c22badf6b486a863d6aeecc
LYK <dalinaum@gmail.com>
2024-09-15 02:10:28 +0900

    불필요한 메서드를 제거하고 주석을 추가.

--- client/src/queryClient.ts
+++ client/src/queryClient.ts
@@ -23,43 +23,9 @@ export const getClient = (() => {
 
 const BASE_URL = import.meta.env.VITE_SERVER_BASE_URL || 'http://localhost:8000/graphql'
 
-export const resetFetcher = async ({
-    method,
-    path,
-    body,
-    params,
-}: {
-    method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
-    path: string;
-    body?: AnyOBJ;
-    params?: AnyOBJ;
-}) => {
-    try {
-        let url = `${BASE_URL}${path}`
-        const fetchOptions: RequestInit = {
-            method,
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': BASE_URL
-            }
-        }
-        if (params) {
-            const searchParams = new URLSearchParams(params)
-            url += '?' + searchParams.toString()
-        }
-        if (body) {
-            fetchOptions.body = JSON.stringify(body)
-        }
-        const res = await fetch(url, fetchOptions)
-        const json = await res.json()
-        return json
-    } catch (err) {
-        console.error(err)
-    }
-}
-
 export const graphqlFetcher = <T>(query: RequestDocument, variables = {}) =>
     request<T>(BASE_URL, query, variables)
+// TODO: 강의에서는 Acceess-Control-Allow-Origin을 넣는데 그건 잘못인 것 같고, Origin은 다음에 넣어보자.
 
 export const QueryKeys = {
     PRODUCTS: 'PRODUCTS',
